1f872534412dfeefb32f4b5e75be0aeebb7c722b
Sui Generis <natsynapses@gmail.com>
2020-05-20 15:31:47 -0500

    cleaning
    up

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
+/** SERVER APP: OrgBoat **
+ ██████  ██████   ██████  ██████   ██████   █████  ████████ 
+██    ██ ██   ██ ██       ██   ██ ██    ██ ██   ██    ██    
+██    ██ ██████  ██   ███ ██████  ██    ██ ███████    ██    
+██    ██ ██   ██ ██    ██ ██   ██ ██    ██ ██   ██    ██    
+ ██████  ██   ██  ██████  ██████   ██████  ██   ██    ██    
+                                                                                                                                                                                                         
+*** CODED BY sui Gn
+workspace
+****/
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const jwt = require('jsonwebtoken')
+const config = require('./configs/config')
+const { body,validationResult } = require("express-validator")
+const { sanitizeBody } = require("express-validator")
+var bodyParser = require("body-parser")
+var routes = require('./routes')
+var unicorn = "🍺🦄🍺"
+var uuid = require('node-uuid')
+const { Client } = require('pg')
+//const theVault = new Client({
+//connectionString: "postgres://csicplnifqncpc:ce12c51c83e437148779a4f7e0d508722f0a5ce9f05f894f9b6f88b9f2d9b3f9@ec2-174-129-253-53.compute-1.amazonaws.com:5432/d70qi6m3chd89a",
+//ssl: true,
+//});
+//theVault.connect();
+const rutasProtegidas = express.Router(); 
+rutasProtegidas.use((req, res, next) => {
+    const token = req.headers['access-token'];
+ 
+    if (token) {
+      jwt.verify(token, app.get('llave'), (err, decoded) => {      
+        if (err) {
+          return res.json({ mensaje: 'Token inválida' });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.send({ 
+          mensaje: 'Token no proveída.' 
+      });
+    }
+ });
 
-express()
+const server = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/testing', (req, res) => res.render('pages/index'))
+  .get('/', routes.home)
+  .post('/login', routes.login)
+  .get('/datos', rutasProtegidas, routes.datos)
+  .post('/subscribing', routes.subscribing)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
