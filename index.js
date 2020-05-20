b082e86b0baf725fd012c45b4af2d39e81023f53
Sui Generis <natsynapses@gmail.com>
2020-05-20 13:47:54 -0500

    going live
    going live

---
+++ index.js
@@ -0,0 +1,225 @@
+/** SERVER APP: OrgBoat **
+ ██████  ██████   ██████  ██████   ██████   █████  ████████ 
+██    ██ ██   ██ ██       ██   ██ ██    ██ ██   ██    ██    
+██    ██ ██████  ██   ███ ██████  ██    ██ ███████    ██    
+██    ██ ██   ██ ██    ██ ██   ██ ██    ██ ██   ██    ██    
+ ██████  ██   ██  ██████  ██████   ██████  ██   ██    ██    
+                                                                                                                                                                                                         
+*** CODED BY sui Gn
+workspace
+****/
+//SETTING UP SERVER VARIABLES AND DEPENDENCIES
+/** Global variables */
+const forceSecure = require("force-secure-express");
+const express = require('express');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const config = require('./configs/config');
+var nodemailer = require('nodemailer');
+const PORT = process.env.PORT || 3000;
+//const PORT = 31416; //Cleaking
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+var bodyParser = require("body-parser");
+var routes = require('./routes');
+var unicorn = "🍺🦄🍺";
+var uuid = require('node-uuid');
+const { Client } = require('pg');
+const theVault = new Client({
+connectionString: "postgres://csicplnifqncpc:ce12c51c83e437148779a4f7e0d508722f0a5ce9f05f894f9b6f88b9f2d9b3f9@ec2-174-129-253-53.compute-1.amazonaws.com:5432/d70qi6m3chd89a",
+ssl: true,
+});
+theVault.connect();
+const rutasProtegidas = express.Router(); 
+rutasProtegidas.use((req, res, next) => {
+    const token = req.headers['access-token'];
+ 
+    if (token) {
+      jwt.verify(token, app.get('llave'), (err, decoded) => {      
+        if (err) {
+          return res.json({ mensaje: 'Token inválida' });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.send({ 
+          mensaje: 'Token no proveída.' 
+      });
+    }
+ });
+ 
+const server = express()
+//Middleware session
+	//SETTING UP ROUTING SPECS
+ 	.use(bodyParser.urlencoded({ extended: false }))
+ 	.use(bodyParser.json())
+	//.use(forceSecure(["cleaker.me","wwww.cleaker.me"])) // FORCE SSL
+	.use(express.static(path.join(__dirname, 'server/public')))
+	.set('views', path.join(__dirname, 'server/views'))
+	.set('view engine', 'ejs')
+	.set('llave', config.llave)
+	//ROUTING OrgBoat
+	.get('/', routes.home)
+	.post('/login', routes.login)
+	.get('/datos', rutasProtegidas, routes.datos)
+	.post('/subscribing', routes.subscribing)
+	.listen(PORT, () => console.log(`Cleaker on PORT: ${ PORT }
+	freelanding ${ unicorn }`));
+	
+
+	
+		//      _ ___   _  _  __
+		//  |V||_  ||_|/ \| \(_ 
+		//  | ||__ || |\_/|_/__)	
+			
+			function brdCstRight(room, obj){ //broadcast to room membrs Only
+			var BroadCastMembers = [ ];
+	     	//Filters only members belonging to the same room
+			const members = allMembers.filter(goes => goes.room === room);
+			//Once filtered to only same room members to broadcast
+			members.forEach(function(element) {BroadCastMembers.push(element.client);});
+			// broadcast message to all connected clients in room
+			BroadCastMembers.forEach(function(EndClient){EndClient.sendUTF(obj);});	
+				};
+				
+		/** Helper function for escaping input strings */
+		function htmlEntities(str) {
+			return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
+						      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+								}			
+								// Array with some colors
+								var colors = [ '#a8d069', '#30ad64', '#25ccbf', '#20ac99', '#f8c740', '#e2a62b',
+								 '#face6a', '#e4b962', '#fd7072', '#cf404d', '#d39f9a', 
+								'#735260', '#af4173', '#822e50', '#e64c40', '#bf3a30','#fc7d64','#49647b'];
+		// ... in random order
+		colors.sort(function(a,b) { return Math.random() > 0.5; } );
+		
+		//REGEXS
+		function emailRegex(email) { //EMAIL REGEX
+			var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return re.test(email);
+		}
+		function usrnmRegex(usrname){ //USERNAME REGEX
+			var re = /^(?=[a-zA-Z0-9._]{5,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+			return re.test(usrname);
+		}
+		function nameRegex(subname){ //NAME REGEX
+			var re = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+			return re.test(subname);
+		}	
+		
+		//NODE.JS EMAILS
+		
+		
+	   /** 				  o       o                                
+						  |       |                               
+						  o   o   o  
+ 	   					   \ / \ / 
+ 	   					    o   o  */
+		/*_      _____ ___ ___  ___   ___ _  _____ _____ 
+		 \ \    / / __| _ ) __|/ _ \ / __| |/ / __|_   _|
+	      \ \/\/ /| _|| _ \__ \ (_) | (__| ' <| _|  | |  
+		   \_/\_/ |___|___/___/\___/ \___|_|\_\___| |_|
+	
+				serverside websocket managment **/
+		var clients = [ ];
+		var allMembers = [ ];
+		var webSocketServer = require('websocket').server;		
+		var wsServer = new webSocketServer({
+	    httpServer: server
+			});
+	//exports.wsServer;
+	// WebSocket server Starts from Here
+	wsServer.on('request', function(request) {
+			   var uuid_numbr = uuid.v4();
+			//accept connection if check 'request.origin'
+			   var connection = request.accept(); //connecting from same website
+			   var index = clients.push(connection) - 1; //client index to remove them on 'close' event
+			   //A connection was acepted.
+			   //console.log('1. wsOnRqstLog - Connection Accepted UUID: ' + uuid_numbr + ' Request.Origin: ' + request.origin);
+			    //starts - comunication with user - connection.on 
+			   connection.sendUTF(JSON.stringify({ type:'cleaked', uuid: uuid_numbr})); // 'cleaked' -- cleaker.js handshake innitiation
+			   //Listening - on incoming comunication
+				  connection.on('message', function(message) {
+					if (message.type === 'utf8') { //IF TEXT. 
+						pckr = JSON.parse(message.utf8Data); //parse to json
+						if (pckr.clkcd === 'CleakerRunMe') { //Create rooms for Broadcast Redirection.
+	   					var runMeMember = {
+	   						room: pckr.cleakerRoom,
+	   						index: index,
+	   						client: connection,
+							uuid: uuid_numbr
+						}	
+	   			  //Push into the array
+	   			  allMembers.push(runMeMember) - 1;// index to remove them on 'close' event;			
+				} 
+				else if (pckr.clkcd === 'onCleaker'){ //CLEAKER NETWORK MONITORING
+				//console.log(pckr.cleaker); //for dev purposes, remove to not saturate the console.
+				//packet - send INFORMATION TO RUNME
+				var activeUser = JSON.stringify({ type: "clkr_Start", cleaker: pckr.cleaker});
+				//console.log(pckr.cleaker);
+				brdCstRight("runmeMasterMind", activeUser);
+						} //ACTIVE USERS - RUNME CLOSURE
+				else if (pckr.clkcd === 'appCleaker'){ // RECEIVING CLEAKER FROM A MOBILE APP
+						//console.log(pckr.cleaker);
+						}//MOBILE APP CLOSURE
+				else if (pckr.clkcd === 'keepMeAlive'){ // TIMER TO KEEP SESSIONS ALIVE
+						//console.log("keepme");
+				 	 	var stayingAlive = JSON.stringify({ type: "stayingAlive", chorus: "A A A A"});
+				 	 	brdCstRight("runmeMasterMind", stayingAlive);
+						}// KEEP ME ALIVE CLOSURE
+				else if (pckr.clkcd == 'vName'){//Verify Name
+						//console.log(pckr.value); //for dev
+						if (nameRegex(pckr.value)) { //Validate name
+						//console.log("Valid Name"); // for dev
+						connection.sendUTF(JSON.stringify({ type:'validDataRes', value: "", rcolor:"#39D1BB",input: "inputName",label:"#labelName"}));
+						} else {
+						//console.log("Invalid Name");
+						connection.sendUTF(JSON.stringify({type:'validDataRes', value: "Invalid Name.", rcolor:"#ff6666",input: "inputName",label:"#labelName"}));
+							}    
+						}// VERIFY NAME CLOSURE
+				else if (pckr.clkcd == 'vUser'){//Verify Username
+						//console.log(pckr.value); //for dev
+						if (usrnmRegex(pckr.value)) { //Regex Username
+						theVault.query('SELECT Usrname FROM Usrs WHERE Usrname = $1', [pckr.value], (err, res) => {
+						if(res.rowCount >= 1){
+							connection.sendUTF(JSON.stringify({ type:'validDataRes', value: "Username already taken.", rcolor:"#ff6666",input: "inputUsername", label: "#labelUsername"}));
+						//console.log("Username already taken!"); //for dev 
+						 }else{	
+						//console.log("valid Username"); // for dev
+						connection.sendUTF(JSON.stringify({type:'validDataRes', value: "", rcolor:"#39D1BB", input: "inputUsername", label: "#labelUsername"}));
+							}})}
+						else{
+						//console.log("invalid Username");
+						connection.sendUTF(JSON.stringify({ type:'validDataRes', value: "Invalid Username.", rcolor:"#ff6666", input: "inputUsername", label: "#labelUsername"}));
+							}    
+						}// VERIFY USERNAME CLOSURE		
+				else if (pckr.clkcd == 'vEmail'){//verify Email Entry
+						//console.log(pckr.value); //for dev 
+						if (emailRegex(pckr.value)) {
+						theVault.query('SELECT Email FROM Usrs WHERE Email = $1', [pckr.value], (err, res) => {
+						if(res.rowCount >= 1){
+						connection.sendUTF(JSON.stringify({type:'validDataRes', value: "Email already taken.", rcolor: "#ff6666", input: "inputEmail", label: "#labelEmail"}));
+						//console.log("Email already taken!");
+						}else{	
+						//console.log("valid Email"); // for dev
+						connection.sendUTF(JSON.stringify({ type:'validDataRes', value: "", rcolor:"#39D1BB",input: "inputEmail",label: "#labelEmail"}));
+						}})}
+						else{
+						connection.sendUTF(JSON.stringify({type:'validDataRes', value: "Invalid Email.", rcolor:"#ff6666", input: "inputEmail", label: "#labelEmail"}));
+								}
+							}//VERIFY if pckr.clkcd == 'verEmEx' EMAIL CLOSURE
+
+
+
+						}//IF MESSAGE.TYPE CLOSURE
+								});//END CONNECTION.ON MESSAGE		
+															
+				// User disconnected
+				connection.on('close', function(connection) {
+						//console.log(".disconnected - UUID:" + uuid_numbr);//logoutRecord
+						clients.splice(index, 1);// remove user from the list of connected clients
+						}); 
+					}); // FINISHES WEB SERVER ON
---
+++ routes.js
@@ -0,0 +1,120 @@
+/*
+______ _____ _   _ _____ _____ _____ 
+| ___ \  _  | | | |_   _|  ___/  ___|
+| |_/ / | | | | | | | | | |__ \ `--. 
+|    /| | | | | | | | | |  __| `--. \
+| |\ \\ \_/ / |_| | | | | |___/\__/ /
+\_| \_|\___/ \___/  \_/ \____/\____/ 
+							OrgBoat
+CODED BY: SUI GENERIS 
+Where do we go from here?
+*/
+
+//DATA BASE CONNECTION
+var index = require('./index');
+const { Client } = require('pg');
+const config = require('./configs/config');
+const theVault = new Client({
+connectionString: "postgres://csicplnifqncpc:ce12c51c83e437148779a4f7e0d508722f0a5ce9f05f894f9b6f88b9f2d9b3f9@ec2-174-129-253-53.compute-1.amazonaws.com:5432/d70qi6m3chd89a",
+ssl: true,
+});
+var uuid = require('node-uuid');
+const jwt = require('jsonwebtoken');
+theVault.connect();
+/* POSTGRES QUERY , VERIFICATION AND SAVING DATA FUNCTION TO THE VAULT 
+	//Verifies if the channel doesn't already exists
+	theVault.query('SELECT channel, channelhash FROM wtmchannels WHERE channel = $1 AND channelhash = $2', [channel, channelHash], (err, res) => {
+	if(res.rowCount == 1){
+		console.log("Channel Already in The Vault Database Records!");
+			}else{
+	//STORES DATA INTO WTM CHANNELS
+	theVault.query('INSERT INTO wtmchannels (channel, channelhash) VALUES ($1, $2)', [channel, channelHash], (error, results) => {
+		     if (error) {
+		     throw error
+		     	 }
+			console.log("New Channel saved!")
+				})//closes query
+			} //closes else
+						}) 
+				*/
+ 
+exports.home = function(req, res){res.render('pages/main/index')};
+exports.shadow = function(req, res){res.render('pages/main/shadow')};
+exports.login = function(req, res){
+	theVault.query('SELECT Usrname, Password FROM Usrs WHERE Usrname = $1 && Password = $2', [req.body.usrname, req.body.pwd], (err, res) => {
+	if(res.rowCount >= 1){
+		res.redirect('/');
+		}
+    if(req.body.usrname === "asfo" && req.body.pwd === "holamundo") {
+  const payload = {
+   check:  true
+  };
+  const token = jwt.sign(payload, config.llave, {
+   expiresIn: 1440
+  });
+  res.json({
+   mensaje: 'Logged In!',
+   token: token
+  });
+    } else {
+        res.json({ mensaje: "Incorrect username or password!"})
+    }
+})
+}
+
+exports.datos = function(req, res){
+ const datos = [
+  { id: 1, nombre: "Asfo" },
+  { id: 2, nombre: "Denisse" },
+  { id: 3, nombre: "Carlos" }
+ ];
+ 
+ res.json(datos);
+};
+
+//subscribe
+exports.subscribing = function(req,res){
+	var clName = req.body.subName; 
+	var usrname = req.body.subUsername; 
+	var email = req.body.subEmail; 
+	var pwd = req.body.subPwd; 
+	var rtPwd = req.body.subRtPwd; 
+	var uuid_numbr = uuid.v4();
+	var verified = 0;
+	var dt = new Date();
+	if (clName <= 3 || usrname <= 4 || email <= 3 || pwd <= 3){
+	}else if (pwd != rtPwd){
+	return false;
+	}else{
+	//Verifies if the user already exists
+	theVault.query('SELECT Usrname FROM Usrs WHERE Usrname = $1', [usrname], (err, res) => {
+	if(res.rowCount >= 1){
+		res.redirect('/');
+		}else{	
+	theVault.query('SELECT Email FROM Usrs WHERE Email = $1', [email], (err, res) => {
+	if(res.rowCount >= 1){
+		res.redirect('/');
+		}else{				
+	//STORES DATA
+	theVault.query('INSERT INTO usrs (uuid, name, usrname, email, password, Verified, last_update) VALUES ($1, $2, $3, $4, $5, $6, $7)', [uuid_numbr, clName, usrname, email, pwd, verified, dt], (error, results) => {
+		if (error) {
+		throw error
+				 }
+		console.log("New user saved!");
+		console.log(clName + usrname + email + pwd);
+			})//closes Insert New Usr Into Table
+			}//else
+			})// Closes second query - email
+				} //closes else first query 
+				}) //closes the vault first query - username
+			}// Pwd do not match
+			res.render('pages/main/runme');
+				}//End Post Method	
+							
+//CLEAKER ANALYTICS ROUTES
+exports.runme = function(req, res){res.render('pages/main/runme')};
+
+
+
+
+
