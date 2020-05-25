045d486296de07884a83f08f46e8dbd395f54a0c
Sui Generis <natsynapses@gmail.com>
2020-05-25 01:21:45 -0500

    login
    login

--- routes.js
+++ routes.js
@@ -15,25 +15,48 @@ var uuid = require('node-uuid');
 const jwt = require('jsonwebtoken');
 const config = require('./configs/config');
 const method = require('./methods');
+const bcrypt = require('bcrypt');
+
 
 exports.home = function(req, res){res.render('pages/index', { opt1: "Sign Up", opt2: "/subscribe", opt3: " " })};
 exports.subscribe = function(req, res){res.render('pages/subscribe', { opt: " ", opt1: "Log In", opt2: "/" })};
 exports.login = function(req, res){
-	 if(req.body.usrname === "asfo" && req.body.pwd === "holamundo") {
-	     const payload = {
-	      check:  true
-	     };
-	     const token = jwt.sign(payload, config.llave, {
-	      expiresIn: 1440
-	     });
-	     res.json({
-	      mensaje: 'Autenticación correcta',
-	      token: token
-	     });
-	       } else {
-			   res.render('pages/index', { opt1: "Sign Up", opt2: "/subscribe", opt3: "Incorrect usarname or password. " })
-	       }
-	   };
+	var usrname = req.body.usrname;
+	var pwd = req.body.pwd;	
+	index.orgboatDB.query('SELECT * FROM Usrs WHERE Usrname = $1 OR email = $1', [usrname], (err, resp) => {
+	    if (err) {
+	     res.render('pages/index', { opt1: "Sign Up", opt2: "/subscribe", opt3: "No account found." });
+	          } else {
+	              // selects return an array, so access the first in the array
+				  var usr = resp.rows[0];
+				  var hash = usr.password;
+	              // now lets compare the passwords
+			  	bcrypt.compare(pwd, hash, function(err, isMatch) {
+			  	  if (err) {
+			  	    throw err
+			  	  } else if (!isMatch) {
+			  	   res.render('pages/index', { opt1: "Sign Up", opt2: "/subscribe", opt3: "Invalid username or password." });
+			  	  } else {
+				 	     const payload = {
+				 	      check:  true
+				 	     };
+				 	     const token = jwt.sign(payload, config.llave, {
+				 	      expiresIn: 1440
+				 	     });
+				 	     res.json({
+				 	      mensaje: 'Autenticación correcta',
+				 	      token: token
+				 	     });
+						 
+				 	       } 
+				 	   })
+			  }
+			  })
+			};
+			
+ 
+	
+
 
 exports.datos = function(req, res){
  const datos = [
@@ -57,6 +80,21 @@ exports.subscribing = function(req,res){
 	var usrname = req.body.subUsername; 
 	var email = req.body.subEmail; 
 	var pwd = req.body.subPwd; 
+	const saltRounds = 10
+	var hashPwd = "";
+	bcrypt.genSalt(saltRounds, function (err, salt) {
+	  if (err) {
+	    throw err
+	  } else {
+	    bcrypt.hash(pwd, salt, function(err, hash) {
+	      if (err) {
+	        throw err
+	      } else {
+			  hashPwd = hash;
+	      }
+	    })
+	  }
+	})
 	var rtPwd = req.body.subRtPwd; 
 	var uuid_numbr = uuid.v4();
 	var verified = 0;
@@ -78,12 +116,12 @@ exports.subscribing = function(req,res){
 			return;
 				}else{				
 		//STORES DATA
-		index.orgboatDB.query('INSERT INTO usrs (name, usrname, email, password, Verified, last_update, u_id, created) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [clName, usrname, email, pwd, verified, dt, uuid_numbr, dt], (error, results) => {
+		index.orgboatDB.query('INSERT INTO usrs (name, usrname, email, password, Verified, last_update, u_id, created) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [clName, usrname, email, hashPwd, verified, dt, uuid_numbr, dt], (error, results) => {
 		if (error) {
 		throw error
 				 }
 		console.log("New user saved!");
-		console.log(clName + usrname + email + pwd);
+		console.log(clName + usrname + email);
 			})//closes Insert New Usr Into Table
 			}//else
 			})// Closes second query - email
