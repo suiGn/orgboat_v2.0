67763532b4fe87b8f1845104e64e096a5fabcb5f
Sui Generis <natsynapses@gmail.com>
2020-05-24 22:28:28 -0500

    susbcribe done
     lighthouse, connect to database, data validation, reset layout, lock screen layout

---
+++ methods.js
@@ -0,0 +1,61 @@
+//      _ ___   _  _  __
+//  |V||_  ||_|/ \| \(_ 
+//  | ||__ || |\_/|_/__)	
+//orgboat
+	//dependencies	
+const index = require('./index');
+
+
+//REGEXS
+//EMAIL REGEX
+const emailRegex = (email) => {
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+}
+exports.emailRegex = emailRegex;
+//USERNAME REGEX
+const usrnmRegex = (usrname) => {
+	var re = /^(?=[a-zA-Z0-9._]{5,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+	return re.test(usrname);
+}
+exports.usrnmRegex = usrnmRegex;
+//NAME REGEX
+const nameRegex = (subname) => {
+	var re = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{3,21}$/u;
+	return re.test(subname);
+}	
+exports.nameRegex = nameRegex;
+
+exports.dataSubmitVerification = function dataSubmitVerification(pckr){
+	if (pckr.code == 'vName'){//Verify Name
+			if (nameRegex(pckr.value)) { //Validate name
+				index.subVerificationRes("validDataRes", "", "#39D1BB", "inputName", "#labelName", "g");//ValidName
+			} else {
+			index.subVerificationRes("validDataRes", "Invalid Name!", "#ff6666", "inputName", "#labelName", "ni");
+				}    
+			}// VERIFY NAME CLOSURE
+	else if (pckr.code == 'vUser'){//Validate Username
+			if (usrnmRegex(pckr.value)) {
+			index.orgboatDB.query('SELECT usrname FROM usrs WHERE usrname = $1', [pckr.value], (err, res) => {
+			if(res.rowCount >= 1){
+			index.subVerificationRes("validDataRes", "Username already taken!", "#ff6666", "inputUsername", "#labelUsername", "ut");
+			 }else{	
+			index.subVerificationRes("validDataRes", "", "#39D1BB", "inputUsername", "#labelUsername", "g");//validUsername
+				}})}
+			else{
+			index.subVerificationRes("validDataRes", "Invalid Username!", "#ff6666", "inputUsername", "#labelUsername", "ui");
+				}    
+			}// VALID USERNAME CLOSURE		
+	else if (pckr.code == 'vEmail'){//Validate Email
+			if (emailRegex(pckr.value)) {
+			index.orgboatDB.query('SELECT email FROM Usrs WHERE Email = $1', [pckr.value], (err, res) => {
+			if(res.rowCount >= 1){
+			index.subVerificationRes("validDataRes", "Email already taken!", "#ff6666", "inputEmail", "#labelEmail", "et");	
+			}else{	
+			index.subVerificationRes("validDataRes", "", "#39D1BB", "inputEmail", "#labelEmail", "g");//validEmail
+				}})}
+			else{
+			index.subVerificationRes("validDataRes", "Invalid Email!", "#ff6666", "inputEmail", "#labelEmail", "ei");	
+					}
+				}//VALID EMAIL CLOSURE
+}
--- routes.js
+++ routes.js
@@ -10,41 +10,30 @@ CODED BY: SUI GENERIS
 where do we go from here?
 */
 
-//DATA BASE CONNECTION
 var index = require('./index');
-const { Client } = require('pg');
-const config = require('./configs/config');
-//const theVault = new Client({
-//connectionString: "postgres://csicplnifqncpc:ce12c51c83e437148779a4f7e0d508722f0a5ce9f05f894f9b6f88b9f2d9b3f9@ec2-174-129-253-53.compute-1.amazonaws.com:5432/d70qi6m3chd89a",
-//ssl: true,
-//});
-//theVault.connect();
 var uuid = require('node-uuid');
 const jwt = require('jsonwebtoken');
-exports.home = function(req, res){res.render('pages/index', { opt1: "Sign Up", opt2: "/subscribe" })};
-exports.subscribe = function(req, res){res.render('pages/subscribe', { opt1: "Log In", opt2: "/" })};
-exports.workspace = function(req, res){res.render('pages/workspace')};
+const config = require('./configs/config');
+const method = require('./methods');
+
+exports.home = function(req, res){res.render('pages/index', { opt1: "Sign Up", opt2: "/subscribe", opt3: " " })};
+exports.subscribe = function(req, res){res.render('pages/subscribe', { opt: " ", opt1: "Log In", opt2: "/" })};
 exports.login = function(req, res){
-	theVault.query('SELECT Usrname, Password FROM Usrs WHERE Usrname = $1 && Password = $2', [req.body.usrname, req.body.pwd], (err, res) => {
-	if(res.rowCount >= 1){
-		res.redirect('/');
-		}
-    if(req.body.usrname === "asfo" && req.body.pwd === "holamundo") {
-  const payload = {
-   check:  true
-  };
-  const token = jwt.sign(payload, config.llave, {
-   expiresIn: 1440
-  });
-  res.json({
-   mensaje: 'Logged In!',
-   token: token
-  });
-    } else {
-        res.json({ mensaje: "Incorrect username or password!"})
-    }
-})
-}
+	 if(req.body.usrname === "asfo" && req.body.pwd === "holamundo") {
+	     const payload = {
+	      check:  true
+	     };
+	     const token = jwt.sign(payload, config.llave, {
+	      expiresIn: 1440
+	     });
+	     res.json({
+	      mensaje: 'Autenticación correcta',
+	      token: token
+	     });
+	       } else {
+			   res.render('pages/index', { opt1: "Sign Up", opt2: "/subscribe", opt3: "Incorrect usarname or password. " })
+	       }
+	   };
 
 exports.datos = function(req, res){
  const datos = [
@@ -56,6 +45,12 @@ exports.datos = function(req, res){
  res.json(datos);
 };
 
+exports.resetPass = function(req, res){res.render('pages/sec/reset-pass')};
+exports.lockScreen= function(req, res){res.render('pages/sec/lock-screen')};
+exports.workspace = function(req, res){res.render('pages/workspace')};
+
+
+
 //subscribe
 exports.subscribing = function(req,res){
 	var clName = req.body.subName; 
@@ -66,21 +61,24 @@ exports.subscribing = function(req,res){
 	var uuid_numbr = uuid.v4();
 	var verified = 0;
 	var dt = new Date();
-	if (clName <= 3 || usrname <= 4 || email <= 3 || pwd <= 3){
+	if (method.nameRegex(clName) && method.usrnmRegex(usrname) && method.emailRegex(email)) {
+	if (clName.length <= 3 || usrname.length <= 3 || email.length <= 3 || pwd.length <= 4){
+	res.render("pages/subscribe" ,{ opt: "Too short."});
 	}else if (pwd != rtPwd){
-	return false;
+	res.redirect("/");
 	}else{
 	//Verifies if the user already exists
-	theVault.query('SELECT Usrname FROM Usrs WHERE Usrname = $1', [usrname], (err, res) => {
-	if(res.rowCount >= 1){
-		res.redirect('/');
-		}else{	
-	theVault.query('SELECT Email FROM Usrs WHERE Email = $1', [email], (err, res) => {
-	if(res.rowCount >= 1){
-		res.redirect('/');
-		}else{				
-	//STORES DATA
-	theVault.query('INSERT INTO usrs (uuid, name, usrname, email, password, Verified, last_update) VALUES ($1, $2, $3, $4, $5, $6, $7)', [uuid_numbr, clName, usrname, email, pwd, verified, dt], (error, results) => {
+	index.orgboatDB.query('SELECT usrname FROM usrs WHERE Usrname = $1', [usrname], (err, resp) => {
+	if(resp.rowCount >= 1){
+		console.log("username exists");
+		return;
+			}else{	
+			index.orgboatDB.query('SELECT Email FROM Usrs WHERE Email = $1', [email], (err, resp) => {
+			if(resp.rowCount >= 1){
+			return;
+				}else{				
+		//STORES DATA
+		index.orgboatDB.query('INSERT INTO usrs (name, usrname, email, password, Verified, last_update, u_id, created) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [clName, usrname, email, pwd, verified, dt, uuid_numbr, dt], (error, results) => {
 		if (error) {
 		throw error
 				 }
@@ -92,6 +90,8 @@ exports.subscribing = function(req,res){
 				} //closes else first query 
 				}) //closes the vault first query - username
 			}// Pwd do not match
-			res.render('pages/main/runme');
-				}//End Post Method	
-							
+			res.render('pages/workspace');
+				}else{
+				res.render("pages/subscribe" ,{ opt: "Oops! Something went wrong while submitting the form."});
+			}
+		}
