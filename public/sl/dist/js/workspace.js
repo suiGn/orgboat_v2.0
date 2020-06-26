f4d90f8200281df55a71eb59a2730d171f25d76a
aldoglez24 <aldoalejandrog@gmail.com>
2020-06-25 23:58:00 -0500

    groups

--- public/sl/dist/js/workspace.js
+++ public/sl/dist/js/workspace.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
 
 
     var socket = io();
-    var sku;
     var chats;
     var my_uid;
     var currentPage = 0;
@@ -34,7 +33,6 @@ $(document).ready(function () {
 
                     var chat_initial;
                     var chat_name;
-                    var chat_time;
 
                     if (chat.chat_u_id_1 != my_uid) {
                         chat_name = chat.user1_name;
@@ -96,7 +94,70 @@ $(document).ready(function () {
                         </li>
 
                     `)
+                
+                
                 } else if (chat.chat_type == 1) {
+
+
+                    var chat_name = chat.chat_name;
+                    var chat_initial = chat_name.substring(0, 1)
+                    var timeMessage = new Date(chat.last_message_time);
+                    var timeLabel;
+                    var today = new Date();
+                    var yesterday = new Date();
+                    yesterday.setDate(yesterday.getDate() - 1);
+
+                    if (timeMessage.getDate() == today.getDate() && timeMessage.getMonth() == today.getMonth() && timeMessage.getFullYear() == today.getFullYear()) {
+                        timeLabel = timeformat(timeMessage);
+                    } else if (timeMessage.getDate() == yesterday.getDate() && timeMessage.getMonth() == yesterday.getMonth() && timeMessage.getFullYear() == yesterday.getFullYear()) {
+                        timeLabel = "Ayer";
+                    } else {
+                        timeLabel = getDateLabel(timeMessage)
+                    }
+
+
+
+                    $("#chats-list").append(`    
+        
+
+                        <li class="list-group-item chat-conversation-select" i='${chat.chat_uid}' n='${chat_name}' t='${timeMessage.getTime()}'>
+                            <div>
+                                <figure class="avatar">
+                                    <span class="avatar-title bg-info rounded-circle">${chat_initial}</span>
+                                </figure>
+                            </div>
+                            <div class="users-list-body">
+                                <div>
+                                    <h5 class = 'last-message-user' i='${chat.chat_uid}'>${chat_name}</h5>
+                                    <p class = 'last-message-chat' i='${chat.chat_uid}'>${chat.last_message_message}</p>
+                                </div>
+                                <div class="users-list-action">
+                                <div class="new-message-count d-none" i='${chat.chat_uid}' style='height:9px; width:9px; margin-bottom: 12px;' ></div>
+                                    <small class="text-muted last-message-time" i='${chat.chat_uid}'>${timeLabel}</small>
+                                    <div class="action-toggle">
+                                        <div class="dropdown">
+                                            <a data-toggle="dropdown" href="#">
+                                                <i data-feather="more-horizontal"></i>
+                                            </a>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <a href="#" class="dropdown-item">Open</a>
+                                                <a href="#" data-navigation-target="contact-information"
+                                                    class="dropdown-item">Profile</a>
+                                                <a href="#" class="dropdown-item">Add to archive</a>
+                                                <div class="dropdown-divider"></div>
+                                                <a href="#" class="dropdown-item text-danger">Delete</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                    `)
+                
+
+
+
 
                 }
 
@@ -353,10 +414,6 @@ $(document).ready(function () {
     });
 
 
-
-
-
-
 });
 
 function reorderChats() {
@@ -386,17 +443,12 @@ function timeformat(date) {
 }
 
 
-
 function getDateLabel(date) {
-
-
 
     dateLabelDate = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
     dateLabelMonth = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
     dateLabelYear = date.getFullYear();
-
     dateLabel = dateLabelDate + '/' + dateLabelMonth + '/' + dateLabelYear;
-
     return dateLabel;
 
 }
