6c4cafdd5567635b9c362c83fc7b6cd3dbeabec5
karlarob <karla@happyshuttlecancun.com>
2020-09-24 17:11:21 -0500

    Avances a Chat

--- socket-io-client/src/components/Chat.js
+++ socket-io-client/src/components/Chat.js
@@ -15,7 +15,7 @@ function Chat() {
   }, [response]);
   chats = response.chats;
   my_uid = response.my_uid;
-  console.log("Respuesta", response)
+  console.log("Respuesta", response);
   function timeformat(date) {
     var h = date.getHours();
     var m = date.getMinutes();
@@ -88,64 +88,20 @@ function Chat() {
       </form>
       <div className="sidebar-body">
         <ul className="list-group list-group-flush">
-          {chats && chats.map((chat) => {
-            if (
-              chat.chat_type == 0 &&
-              chat.archiveChat == 0 &&
-              chat.delete_chat == 0
-            ) {
-              let chat_initial;
-              let chat_name;
-              let p;
-              let chat_with_usr = chat.user_chat;              
-              if (my_uid != chat.user_chat) {
-                chat_name = chat.name;
-                chat_initial = chat_name.substring(0, 1);
-                let timeMessage = new Date(chat.last_message_time);
-                let timeLabel;
-                let today = new Date();
-                let yesterday = new Date();
-                yesterday.setDate(yesterday.getDate() - 1);
-                if (
-                  timeMessage.getDate() == today.getDate() &&
-                  timeMessage.getMonth() == today.getMonth() &&
-                  timeMessage.getFullYear() == today.getFullYear()
-                ) {
-                  timeLabel = timeformat(timeMessage);
-                } else if (
-                  timeMessage.getDate() == yesterday.getDate() &&
-                  timeMessage.getMonth() == yesterday.getMonth() &&
-                  timeMessage.getFullYear() == yesterday.getFullYear()
-                ) {
-                  timeLabel = "Yesterday";
-                } else {
-                  timeLabel = getDateLabel(timeMessage);
-                }
-                //var pphotoUser = new File([""], chat.pphoto);
-                //var p = "";
-                console.log(chat.pphoto);
-                if (chat.pphoto === "") {
-                  p = <span class="avatar-title bg-info rounded-circle">{chat_initial}</span>;
-                } else {
-                  p = <img src={"/pphotoChat/"+ chat_name} class="rounded-circle" alt="image"/>;
-                }
-                return (
-                  <li
-                    className="list-group-item chat-conversation-select"
-                    key={chat.chat_uid}
-                    i={chat.chat_uid}
-                    n={chat_name}
-                    t={timeMessage.getTime()}
-                    u={chat_with_usr}
-                  >
-                    <figure className="avatar">{p}</figure>
-                  </li>
-                );
-              } else if (chat.chat_type == 1) {
-                if (chat.user_chat == chat.last_message_user_uid) {
-                  let chat_name = chat.chat_name;
-                  let chat_with_usr = chat.user_chat;
-                  let chat_initial = chat_name.substring(0, 1);
+          {chats &&
+            chats.map((chat) => {
+              if (
+                chat.chat_type == 0 &&
+                chat.archiveChat == 0 &&
+                chat.delete_chat == 0
+              ) {
+                let chat_initial;
+                let chat_name;
+                let p;
+                let chat_with_usr = chat.user_chat;
+                if (my_uid != chat.user_chat) {
+                  chat_name = chat.name;
+                  chat_initial = chat_name.substring(0, 1);
                   let timeMessage = new Date(chat.last_message_time);
                   let timeLabel;
                   let today = new Date();
@@ -162,76 +118,94 @@ function Chat() {
                     timeMessage.getMonth() == yesterday.getMonth() &&
                     timeMessage.getFullYear() == yesterday.getFullYear()
                   ) {
-                    timeLabel = "Ayer";
+                    timeLabel = "Yesterday";
                   } else {
                     timeLabel = getDateLabel(timeMessage);
                   }
+                  //var pphotoUser = new File([""], chat.pphoto);
+                  //var p = "";
+                  console.log(chat.pphoto);
+                  if (chat.pphoto === "") {
+                    p = (
+                      <span class="avatar-title bg-info rounded-circle">
+                        {chat_initial}
+                      </span>
+                    );
+                  } else {
+                    p = (
+                      <img
+                        src={"/pphotoChat/" + chat_name}
+                        class="rounded-circle"
+                        alt="image"
+                      />
+                    );
+                  }
                   return (
-                  <li
-                    className="list-group-item chat-conversation-select"
-                    key={chat.chat_uid}
-                    i={chat.chat_uid}
-                    n={chat_name}
-                    t={timeMessage.getTime()}
-                    u={chat_with_usr}
-                  >
-                    <div>
-                      <figure className="avatar">
-                        <span className="avatar-title bg-info rounded-circle">
-                          {chat_initial}
-                        </span>
-                      </figure>
-                      <div className="users-list-body">
+                    <li
+                      className="list-group-item chat-conversation-select"
+                      key={chat.chat_uid}
+                      i={chat.chat_uid}
+                      n={chat_name}
+                      t={timeMessage.getTime()}
+                      u={chat_with_usr}
+                    >
+                      <div>
+                        <figure className="avatar">{p}</figure>
+                      </div>
+                      <div class="users-list-body">
                         <div>
-                          <h5
-                            className="last-message-user"
-                            i="${chat.chat_uid}"
-                          >
+                          <h5 class="last-message-user" i={chat.chat_uid}>
                             {chat_name}
                           </h5>
-                          <p className="last-message-chat" i="${chat.chat_uid}">
-                            {chat.name}: {chat.last_message_message}
+                          <p class="last-message-chat" i={chat.chat_uid}>
+                            {chat.last_message_message}
                           </p>
                         </div>
-                      </div>
-                      <div className="users-list-action">
-                        <div
-                          className="new-message-count d-none"
-                          i={chat.chat_uid}
-                        >
+                        <div class="users-list-action">
+                          <div
+                            class="new-message-count d-none"
+                            i={chat.chat_uid}
+                          ></div>
                           <small
-                            className="text-muted last-message-time"
+                            class="text-muted last-message-time"
                             i={chat.chat_uid}
                           >
-                            {timeLabel}
+                            {timeLabel} 111
                           </small>
-                          <div className="action-toggle">
-                            <div className="dropdown">
+                          <div class="action-toggle">
+                            <div class="dropdown">
                               <a data-toggle="dropdown" href="#">
-                                <i className="fa fa-ellipsis-h"></i>
+                                <i class="fa fa-ellipsis-h"></i>
                               </a>
-                              <div className="dropdown-menu dropdown-menu-right">
-                                <a href="#" className="dropdown-item">
+                              <div class="dropdown-menu dropdown-menu-right">
+                                <a href="#" class="dropdown-item">
                                   Open
                                 </a>
                                 <button
-                                  // onClick={profiledata({chat.chat_uid})}
+                                //   onClick="profiledata('${
+                                //   chat.chat_uid
+                                // }')"
                                   data-navigation-target="contact-information"
-                                  className="dropdown-item"
+                                  class="dropdown-item"
                                 >
                                   Profile
                                 </button>
                                 <a
                                   href="#"
-                                  // onClick="ArchiveChat('${chat.chat_uid}')"
-                                  className="dropdown-item"
+                                //   onClick="ArchiveChat('${
+                                //   chat.chat_uid
+                                // }')"
+                                  class="dropdown-item"
                                 >
                                   Add to archive
                                 </a>
-                                <div className="dropdown-divider"></div>
+                                <div class="dropdown-divider"></div>
                                 <a
                                   href="#"
-                                  className="dropdown-item text-danger"
+                                  class="dropdown-item text-danger"
+                                //   onclick="DeleteChat('${
+                                //   chat.chat_uid
+                                // }')"
                                 >
                                   Delete
                                 </a>
@@ -240,14 +214,117 @@ function Chat() {
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </li>
-                   ); 
+                    </li>
+                  );
+                } else if (chat.chat_type == 1) {
+                  if (chat.user_chat == chat.last_message_user_uid) {
+                    let chat_name = chat.chat_name;
+                    let chat_with_usr = chat.user_chat;
+                    let chat_initial = chat_name.substring(0, 1);
+                    let timeMessage = new Date(chat.last_message_time);
+                    let timeLabel;
+                    let today = new Date();
+                    let yesterday = new Date();
+                    yesterday.setDate(yesterday.getDate() - 1);
+                    if (
+                      timeMessage.getDate() == today.getDate() &&
+                      timeMessage.getMonth() == today.getMonth() &&
+                      timeMessage.getFullYear() == today.getFullYear()
+                    ) {
+                      timeLabel = timeformat(timeMessage);
+                    } else if (
+                      timeMessage.getDate() == yesterday.getDate() &&
+                      timeMessage.getMonth() == yesterday.getMonth() &&
+                      timeMessage.getFullYear() == yesterday.getFullYear()
+                    ) {
+                      timeLabel = "Ayer";
+                    } else {
+                      timeLabel = getDateLabel(timeMessage);
+                    }
+                    return (
+                      <li
+                        className="list-group-item chat-conversation-select"
+                        key={chat.chat_uid}
+                        i={chat.chat_uid}
+                        n={chat_name}
+                        t={timeMessage.getTime()}
+                        u={chat_with_usr}
+                      >
+                        <div>
+                          <figure className="avatar">
+                            <span className="avatar-title bg-info rounded-circle">
+                              {chat_initial}
+                            </span>
+                          </figure>
+                          <div className="users-list-body">
+                            <div>
+                              <h5
+                                className="last-message-user"
+                                i={chat.chat_uid}
+                              >
+                                {chat_name}
+                              </h5>
+                              <p
+                                className="last-message-chat"
+                                i={chat.chat_uid}
+                              >
+                                {chat.name}: {chat.last_message_message}
+                              </p>
+                            </div>
+                          </div>
+                          <div className="users-list-action">
+                            <div
+                              className="new-message-count d-none"
+                              i={chat.chat_uid}
+                            >
+                              <small
+                                className="text-muted last-message-time"
+                                i={chat.chat_uid}
+                              >
+                                {timeLabel}
+                              </small>
+                              <div className="action-toggle">
+                                <div className="dropdown">
+                                  <a data-toggle="dropdown" href="#">
+                                    <i className="fa fa-ellipsis-h"></i>
+                                  </a>
+                                  <div className="dropdown-menu dropdown-menu-right">
+                                    <a href="#" className="dropdown-item">
+                                      Open
+                                    </a>
+                                    <button
+                                      // onClick={profiledata({chat.chat_uid})}
+                                      data-navigation-target="contact-information"
+                                      className="dropdown-item"
+                                    >
+                                      Profile
+                                    </button>
+                                    <a
+                                      href="#"
+                                      // onClick="ArchiveChat('${chat.chat_uid}')"
+                                      className="dropdown-item"
+                                    >
+                                      Add to archive
+                                    </a>
+                                    <div className="dropdown-divider"></div>
+                                    <a
+                                      href="#"
+                                      className="dropdown-item text-danger"
+                                    >
+                                      Delete
+                                    </a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    );
                   }
+                }
               }
-            }
-
-          })}
+            })}
         </ul>
       </div>
     </div>
