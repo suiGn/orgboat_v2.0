f5741fa7e6cc3ad73fedc942a0d40ed1297e25de
brunodc <striderreimu94@hotmail.com>
2021-02-04 20:31:59 -0500

    ver grupos

--- src/App/Sidebars/Chats/index.js
+++ src/App/Sidebars/Chats/index.js
@@ -19,8 +19,22 @@ function Index(props) {
     inputRef.current.focus();
     socket.on("retrieve chats", (data) => {
       //TODO: acualizar la lista de unread_messages
+      console.log(data)
+      var chats = data.chats.filter((chats)=>{
+        return chats.chat_type == 0;
+      })
+
+      var grupos = data.chats.filter((grupos)=>{
+        return (grupos.chat_type == 1 && grupos.user_chat == data.my_uid);
+      })
+
+      grupos.forEach((info)=>{
+        info.name = info.chat_name
+      })
+      
+      chats.push.apply(chats,grupos)
       setChatList(data);
-      setfavoriteFriendFiltered(data.chats);
+      setfavoriteFriendFiltered(chats);
     });
   });
   useEffect(() => {
@@ -69,7 +83,7 @@ function Index(props) {
     let chat_name;
     let p;
     let chat_with_usr = chat.user_chat;
-    if (my_uid != chat.user_chat) {
+    if (chat.chat_type==1||(my_uid != chat.user_chat)) {
       chat_name = chat.name;
       chat_initial = chat_name.substring(0, 1);
       let timeMessage = new Date(chat.last_message_time);
