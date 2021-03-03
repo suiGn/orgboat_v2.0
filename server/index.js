a642349e4d2711c82d3912d3e30e6aab3cd39d3c
Sebas19 <sebasjimpuya@hotmail.com>
2021-03-02 22:48:21 -0500

    change email

--- server/index.js
+++ server/index.js
@@ -121,7 +121,7 @@ const server = express()
         signedRequest: data,
         url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
       };
-      res.write(JSON.stringify(returnData));
+      res.json(JSON.stringify(returnData));
       res.end();
     });
   })
