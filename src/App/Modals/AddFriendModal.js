6abc657556391080a1c1b744489f729fb5562a98
Sebas19 <sebasjimpuya@hotmail.com>
2021-03-05 17:55:51 -0500

    mensaje

--- src/App/Modals/AddFriendModal.js
+++ src/App/Modals/AddFriendModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Button,
   Modal,
@@ -32,6 +32,7 @@ function AddFriendModal(props) {
 
   const [email, setEmail] = useState("");
   const [findUsers, setfindUsers] = useState([]);
+  const [validate , setValidate] =  useState(null);
   function SearchUserByEmailOrUserName(e) {
     e.preventDefault();
     var input = email;
@@ -43,6 +44,7 @@ function AddFriendModal(props) {
     });
     props.socket.once("retrive SearchUserByEmailOrUsername", (data) => {
       setfindUsers(data.users);
+      setValidate(data.validate);
     });
     setShowSubmit(false);
   }
@@ -59,9 +61,36 @@ function AddFriendModal(props) {
     });
     notify();
   }
+  useEffect(()=>{
+    if(validate === 1){
+      notifyFriend();
+    }else if(validate === 2){
+      notifyDontExist();
+    }
+  },validate);
 
   const notify = () =>
     toast.success("Usuario agregado con éxito", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  const notifyDontExist = () =>
+    toast.success("Nombre de usuario o correo no encontrado", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  const notifyFriend = () =>
+    toast.success("Usuario ya es amigo", {
       position: "top-right",
       autoClose: 5000,
       hideProgressBar: false,
