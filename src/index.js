f2cd3f6e0eb9eee97455319eaaa677bf80c85be1
jonjefemet <jonshua_97@outlook.com>
2021-04-14 22:56:46 -0500

    ajustes globo de texto y container

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/scss/Index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { Container } from 'reactstrap';
 
-import {Provider} from "react-redux"
+import { Provider } from "react-redux"
 import store from './Store'
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
-    </Provider> ,
+      <Container className="themed-container" fluid="sm">
+          <App />
+      </Container>
+    </Provider>,
     document.getElementById('root')
 );
 
